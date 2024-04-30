d9b07acaacc785479e3fee781f1b7df1cfa6b8d5
Benjamin Bollen <ben@doru.io>
2024-04-30 15:00:39 +0100

    (test/names): cutting short effort to handle decoding base58 in tests

--- test/names/MockNameRegistry.sol
+++ test/names/MockNameRegistry.sol
@@ -5,4 +5,40 @@ import "../../src/names/NameRegistry.sol";
 
 contract MockNameRegistry is NameRegistry {
     constructor() NameRegistry(IHubV2(address(1))) {}
+
+    // External functions
+
+    function registerShortNameNoChecks() external {
+        _registerShortName();
+    }
+
+    function registerShortNameWithNonceNoChecks(uint256 _nonce) external {
+        _registerShortNameWithNonce(_nonce);
+    }
+
+    function registerCustomNameNoChecks(address _avatar, string calldata _name) external {
+        if (bytes(_name).length == 0) {
+            // if name is left empty, it will default to default name "Circles-<base58(short)Name>"
+            return;
+        }
+        if (!isValidName(_name)) {
+            revert CirclesNamesInvalidName(_avatar, _name, 0);
+        }
+        customNames[_avatar] = _name;
+    }
+
+    function registerCustomSymbolNoChecks(address _avatar, string calldata _symbol) external {
+        if (bytes(_symbol).length == 0) {
+            // if symbol is left empty, it will default to default symbol "CRC"
+            return;
+        }
+        if (!isValidSymbol(_symbol)) {
+            revert CirclesNamesInvalidName(_avatar, _symbol, 1);
+        }
+        customSymbols[_avatar] = _symbol;
+    }
+
+    function setCidV0DigestNoChecks(address _avatar, bytes32 _dicV0Digest) external {
+        _setCidV0Digest(_avatar, _dicV0Digest);
+    }
 }
--- test/names/NameRegistry.t.sol
+++ test/names/NameRegistry.t.sol
@@ -4,8 +4,48 @@ pragma solidity >=0.8.24;
 import {Test} from "forge-std/Test.sol";
 import {StdCheats} from "forge-std/StdCheats.sol";
 import "forge-std/console.sol";
+import "../setup/HumanRegistration.sol";
 import "./MockNameRegistry.sol";
+import "./base58Helper.sol";
 
-contract NamesTest is Test {
-    function setUp() public {}
+contract NamesTest is Test, HumanRegistration, Base58Decode {
+    // Constants
+
+    // IPFS hash for Ubuntu 20.04, random CIDv0
+    string constant IPFS_CID_V0 = "QmPK1s3pNYLi9ERiq3BDxKa4XosgWwFRQUydHUtz4YgpqB";
+    // using https://cid.ipfs.tech/#QmPK1s3pNYLi9ERiq3BDxKa4XosgWwFRQUydHUtz4YgpqB
+    // this is the 32 bytes hash digest
+    bytes32 constant DECODED_UINT = 0x0E7071C59DF3B9454D1D18A15270AA36D54F89606A576DC621757AFD44AD1D2E;
+
+    // State variables
+
+    MockNameRegistry mockNameRegistry;
+
+    bytes32 cidBytes;
+
+    // Constructor
+
+    constructor() HumanRegistration(4) {}
+
+    // Setup
+
+    function setUp() public {
+        mockNameRegistry = new MockNameRegistry();
+        // Convert CIDv0 to bytes32 (should be done off-chain in production)
+        console.log("CIDv0 length", bytes(IPFS_CID_V0).length);
+        cidBytes = convertCidV0ToBytes32(IPFS_CID_V0);
+    }
+
+    // Tests
+
+    function testCidV0Digest() public {
+        mockNameRegistry.setCidV0DigestNoChecks(addresses[0], cidBytes);
+    }
+
+    // Helper functions
+
+    function convertCidV0ToBytes32(string memory _cidV0) internal view returns (bytes32) {
+        bytes memory decodedBytes = base58Decode(_cidV0);
+        console.log("CIDv0 length:", decodedBytes.length);
+    }
 }
