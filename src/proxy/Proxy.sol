8547231c76f78277e1456f4031783a76a9a2d2f0
Benjamin Bollen <benjamin.bollen@gnosis.io>
2024-08-07 19:29:38 +0200

    (proxy): make mastercopy storage slot internal

--- src/proxy/Proxy.sol
+++ src/proxy/Proxy.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-// Adapted from https://github.com/gnosis/safe-contracts
+// Adapted from https://github.com/gnosis/safe-contracts (originally)
+// and updated with https://github.com/safe-global/safe-smart-account/blob/main/contracts/proxies/SafeProxy.sol
 pragma solidity >=0.8.4;
-
-import "../errors/Errors.sol";
 
 /// @title IProxy - interface to access master copy of the proxy on-chain
 interface IProxy {
@@ -14,49 +13,30 @@ interface IProxy {
 /// @author Stefan George - <stefan@gnosis.io>
 /// @author Richard Meissner - <richard@gnosis.io>
 contract Proxy {
-    // masterCopy always needs to be first declared variable,
-    // to ensure that it is at the same location in the contracts
-    // to which calls are delegated.
-    // To reduce deployment costs this variable is internal
-    // and needs to be retrieved via `getStorageAt`
-    address public masterCopy;
+    // Singleton always needs to be first declared variable, to ensure that it is at the same location in the contracts to which calls are delegated.
+    // To reduce deployment costs this variable is internal and needs to be retrieved via `getStorageAt`
+    address internal singleton;
 
-    /// @dev Constructor function sets address of master copy contract.
-    /// @param _masterCopy Master copy address.
-    constructor(address _masterCopy) {
-        if (_masterCopy == address(0)) {
-            // Invalid master copy address provided
-            revert();
-        }
-        masterCopy = _masterCopy;
+    /**
+     * @notice Constructor function sets address of singleton contract.
+     * @param _singleton Singleton address.
+     */
+    constructor(address _singleton) {
+        require(_singleton != address(0), "Invalid singleton address provided");
+        singleton = _singleton;
     }
 
-    // todo: consider removing payable
+    /// @dev Fallback function forwards all transactions and returns all received return data.
     fallback() external payable {
-        _fallback();
-    }
-
-    receive() external payable {
-        _fallback();
-    }
-
-    // -- internal functions
-
-    /// @dev Fallback function forwards all transactions and
-    ///      returns all received return data.
-    function _fallback() internal {
         // solhint-disable-next-line no-inline-assembly
         assembly {
-            let _singleton := and(sload(0), 0xffffffffffffffffffffffffffffffffffffffff)
-            // 0xa619486e == keccak("masterCopy()").
-            // The value is right padded to 32-bytes with 0s
-            // solhint-disable-next-line max-line-length
+            let _singleton := sload(0)
+            // 0xa619486e == keccak("masterCopy()"). The value is right padded to 32-bytes with 0s
             if eq(calldataload(0), 0xa619486e00000000000000000000000000000000000000000000000000000000) {
-                mstore(0, _singleton)
+                mstore(0, shr(12, shl(12, _singleton)))
                 return(0, 0x20)
             }
             calldatacopy(0, 0, calldatasize())
-            // solhint-disable-next-line max-line-length
             let success := delegatecall(gas(), _singleton, 0, calldatasize(), 0, 0)
             returndatacopy(0, 0, returndatasize())
             if eq(success, 0) { revert(0, returndatasize()) }
