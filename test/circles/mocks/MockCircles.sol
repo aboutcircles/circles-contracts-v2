0b4a0bfdd8b894a848f348184ea07bcd95b46f0d
roleengineer <roleengineer@gmail.com>
2025-03-10 16:49:47 +0100

    (test-circles): implemented mock circles, exposes internals for testing

--- test/circles/mocks/MockCircles.sol
+++ test/circles/mocks/MockCircles.sol
@@ -1,32 +1,94 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.13;
 
-import "src/circles/Circles.sol";
+import {Circles} from "src/circles/Circles.sol";
 
+/**
+ * @title MockCircles
+ * @notice A mock implementation of the Circles contract, exposing
+ *         certain internal functions for testing purposes.
+ */
 contract MockCircles is Circles {
-    // State variables
+    /**
+     * @notice Deploys the mock contract.
+     * @dev Calls the parent Circles constructor to set the URI
+     *      and the inflationDayZero timestamp.
+     * @param _inflationDayZero The reference day (timestamp) from which inflation is calculated.
+     * @param newuri The initial metadata URI.
+     */
+    constructor(uint256 _inflationDayZero, string memory newuri) Circles(_inflationDayZero, newuri) {}
 
-    // Constructor
-    constructor(uint256 _demurrageBase) Circles(_demurrageBase, "") {}
+    // -------------------------------------------------------------------------
+    // Wrapped internal functions to enable testing
+    // -------------------------------------------------------------------------
 
-    // External functions
-
-    function registerHuman(address _human) external {
-        mintTimes[_human].lastMintTime = uint96(block.timestamp);
+    /**
+     * @notice Public wrapper for the internal `_calculateIssuance` function.
+     * @dev This function calculates the amount of Circles a given avatar can claim,
+     *      along with the start and end of the claimable period.
+     * @param _human The address of the avatar/human to calculate issuance for.
+     * @return issuance The claimable Circles in attoCircles.
+     * @return startPeriod The timestamp that marks the start of the claimable period.
+     * @return endPeriod The timestamp that marks the end of the claimable period.
+     */
+    function calculateIssuance(address _human)
+        external
+        view
+        returns (uint256 issuance, uint256 startPeriod, uint256 endPeriod)
+    {
+        (issuance, startPeriod, endPeriod) = _calculateIssuance(_human);
     }
 
-    function registerHumans(address[] calldata _humans) external {
-        for (uint256 i = 0; i < _humans.length; i++) {
-            mintTimes[_humans[i]].lastMintTime = uint96(block.timestamp);
-        }
+    /**
+     * @notice External wrapper for the internal `_claimIssuance` function.
+     * @dev This function claims the Circles issuance for a specified avatar/human
+     *      and updates the avatar’s last mint time.
+     * @param _human The address of the avatar/human claiming issuance.
+     */
+    function claimIssuance(address _human) external {
+        _claimIssuance(_human);
     }
 
-    function claimIssuance() external {
-        require(mintTimes[msg.sender].lastMintTime != 0, "Circles: Not registered");
-        _claimIssuance(msg.sender);
+    /**
+     * @notice External wrapper for the internal `_mintAndUpdateTotalSupply` function.
+     * @dev Mints a specified value of Circles to an account for a given token ID,
+     *      and updates the recorded discounted total supply.
+     * @param _account The address to receive the newly minted Circles.
+     * @param _id The ID of the token type to mint.
+     * @param _value The amount of tokens to mint (in attoCircles).
+     * @param _data Additional data that might be passed along to a receiver contract.
+     * @param _doAcceptanceCheck Whether to perform the ERC1155 receiver acceptance check.
+     */
+    function mintAndUpdateTotalSupply(
+        address _account,
+        uint256 _id,
+        uint256 _value,
+        bytes memory _data,
+        bool _doAcceptanceCheck
+    ) external {
+        _mintAndUpdateTotalSupply(_account, _id, _value, _data, _doAcceptanceCheck);
     }
 
-    function calculateIssuance(address _human) external view returns (uint256, uint256, uint256) {
-        return _calculateIssuance(_human);
+    /**
+     * @notice External wrapper for the internal `_burnAndUpdateTotalSupply` function.
+     * @dev Burns a specified amount of Circles from an account for a given token ID,
+     *      and updates the recorded discounted total supply.
+     * @param _account The address whose tokens are being burned.
+     * @param _id The ID of the token type to burn.
+     * @param _value The amount of tokens to burn (in attoCircles).
+     */
+    function burnAndUpdateTotalSupply(address _account, uint256 _id, uint256 _value) external {
+        _burnAndUpdateTotalSupply(_account, _id, _value);
+    }
+
+    /**
+     * @notice Public wrapper for the internal `_max` function.
+     * @dev Returns the maximum of two uint256 values.
+     * @param a The first value.
+     * @param b The second value.
+     * @return The greater of `a` and `b`.
+     */
+    function max(uint256 a, uint256 b) external pure returns (uint256) {
+        return _max(a, b);
     }
 }
